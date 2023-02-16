9d63fa315caa5cbef9b22702a32800d48c1b7deb
khoerul-adhayana <khoeruladhayana@gmail.com>
2023-02-16 18:01:02 +0700

    updated

---
+++ src/api/kecamatan/controllers/kecamatan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kecamatan controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::kecamatan.kecamatan');
---
+++ src/api/kecamatan/routes/kecamatan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kecamatan router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::kecamatan.kecamatan');
